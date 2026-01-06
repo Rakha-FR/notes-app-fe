1332580f24ad27167462225a6023809ec803d60a
Rakha Fadilah <raka.fadhilah123@gmail.com>
2026-01-06 19:41:02 +0700

    ganti variable

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    API_BASE_URL: 'http://notes-app:5000/api',
+    API_BASE_URL: 'http://192.168.1.75:5000/api',
     ENDPOINTS: {
         TASKS: '/tasks',
         TASK_BY_ID: (id) => `/tasks/${id}`
